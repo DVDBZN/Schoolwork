64594c4ee96cd40c3d435ad26a5785535737d328
DVDBZN <davebozin@gmail.com>
2021-03-11 17:44:50 -0800

    Add files via upload

---
+++ CS111 Intro to Programming/Assignment 07 - Determining Grades.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your grades.");
+            Console.WriteLine("Do not enter any negative numbers or any numbers above 110.");
+                //I have had teachers that give grades that are more than 100 using bonus questions.
+            Console.WriteLine("When complete, please type \"exit\" to complete the calculation.");
+
+            Console.Write("Enter your grade: ");
+            string gradeString = Console.ReadLine();
+            int grade = int.Parse(gradeString);
+            int counter = 0;
+            int total = grade;
+
+            while (gradeString != "exit")
+            {
+                if (gradeString != "exit")
+                {
+                    Console.Write("Enter your grade: ");
+                    gradeString = Console.ReadLine();
+
+                    counter = counter + 1;
+                    total = total + grade;
+                }
+                if (gradeString == "exit")
+                {
+                    double average =  total / counter;
+                    Console.WriteLine("Your average grade is " + average);
+
+                    if (average >= 90 && average <= 110)
+                    {
+                        Console.WriteLine("Your grade is an A.");
+                    }
+                    if (average >= 80 && average < 90)
+                    {
+                        Console.WriteLine("Your grade is a B.");
+                    }
+                    if (average >= 70 && average < 80)
+                    {
+                        Console.WriteLine("Your grade is a C.");
+                    }
+                    if (average >= 60 && average < 70)
+                    {
+                        Console.WriteLine("Your grade is a D.");
+                    }
+                    if (average < 60)
+                    {
+                        Console.WriteLine("Your grade is an F.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ CS111 Intro to Programming/Useful Programs/Assignment 10 - Wholesale Markup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool something = true;
+            while (something == true)
+            {
+                Console.Write("\nEnter the current item price: ");
+                string priceString = Console.ReadLine();
+                double price = double.Parse(priceString);
+
+                Console.Write("Markup of 5%: {0:C2}", (price * 1.05));
+                Console.WriteLine(". Increase of {0:C2}", (price * .05));
+
+                Console.Write("Markup of 6%: {0:C2}", (price * 1.06));
+                Console.WriteLine(". Increase of {0:C2}", (price * .06));
+
+                Console.Write("Markup of 7%: {0:C2}", (price * 1.07));
+                Console.WriteLine(". Increase of {0:C2}", (price * .07));
+
+                Console.Write("Markup of 8%: {0:C2}", (price * 1.08));
+                Console.WriteLine(". Increase of {0:C2}", (price * .08));
+
+                Console.Write("Markup of 9%: {0:C2}", (price * 1.09));
+                Console.WriteLine(". Increase of {0:C2}", (price * .09));
+
+                Console.Write("Markup of 10%: {0:C2}", (price * 1.10));
+                Console.WriteLine(". Increase of {0:C2}", (price * .10));
+            }
+        }
+    }
+}

cab25e66cc9e13ffae233de84bb74096f8aa4ae5
DVDBZN <davebozin@gmail.com>
2021-03-11 18:28:41 -0800

    Add files via upload

---
+++ CS111 Intro to Programming/25_  In Class Participation 02/9-25.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("| *  *  *  *  *  *||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+             System.Console.WriteLine("|  *  *  *  *  *  |		   	                             	|");
+                System.Console.WriteLine("| *  *  *  *  *  *||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                  System.Console.WriteLine("|  *  *  *  *  *  |				                         |");
+                      System.Console.WriteLine("| *  *  *  *  *  *||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                          System.Console.WriteLine("|  *  *  *  *  *  |			                           	|");
+                              System.Console.WriteLine("| *  *  *  *  *  *||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                                  System.Console.WriteLine("|  *  *  *  *  *  |			                              	|");
+                                      System.Console.WriteLine("| *  *  *  *  *  *||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                                          System.Console.WriteLine("|------------------			                               	|");
+                                              System.Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                                                  System.Console.WriteLine("|					                           	|");
+                                                      System.Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                                                          System.Console.WriteLine("|				                             		|");
+                                                              System.Console.WriteLine("||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||");
+                                                              Console.ReadLine();
+        }
+    }
+}
